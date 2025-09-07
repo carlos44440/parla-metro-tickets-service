0ca60268b84885e90796b7fb5fddb3a4c7fa6512
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2025-09-07 20:52:42 -0300

    feat: add tickets model.

---
+++ parla-metro-tickets-api/src/Models/Tickets.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace parla_metro_tickets_api.src.Models
+{
+    public class Tickets
+    {
+        public int Id { get; set; }
+        public string IdPassenger { get; set; }
+        public DateTime Date { get; set;}
+        public string Type { get; set; }
+        public string Status { get; set; }
+        public decimal AmountPaid { get; set; }
+    }
+}
